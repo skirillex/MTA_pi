1f8717e336d940f9f19f433b722453327167f3e9
Kirill <kirillmasychev@outlook.com>
2020-02-18 23:13:05 -0500

    gui now displays train times but doesn't refresh

--- gui.py
+++ gui.py
@@ -9,28 +9,109 @@ from kivy.uix.image import Image
 
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.graphics import Color, Ellipse
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
+from kivy.core.window import Window
+
+import mta_times
 
 red = [1,0,0,1]
 green = [0,1,0,1]
 blue =  [0,0,1,1]
 purple = [1,0,1,1]
 
+
+class RootWidget(FloatLayout):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        #self.add_widget(Ellipse(text = "hi")) #size_hint=(.5,.5),pos_hint={'center_x':.5, 'center_y':.5}))
+
+###need to figure out way for times to update
+
 class gui(App):
 
     def build(self):
-        #return Label(text = "hello world",size_hint=(.5,.5),pos_hint={"center_x": .6, "center_y": .5})
 
-        #img = Image(source = 'C:/Users/Kiril/PycharmProjects/MTA_pi/1.svg.png', size_hint=(.5, .5), pos_hint={"center_x":.5, "center_y": .5})
-        #return img
-
-        layout = BoxLayout(padding = 10, orientation = "vertical")
         colors = [red, green, blue, purple]
+        trains_list, times_list, = self.get_trains()
+        print(times_list)
+        print(trains_list)
+        #direction = Label(text = "Uptown", size_hint=(.5,.5),halign = "left", font_size=30,pos_hint={"center_x": .1, "center_y": .5})
 
-        for i in range(5):
-            btn = Button(text = "Button #%s" % (i+1),
-            background_color= random.choice(colors))
-            layout.add_widget(btn)
-        return layout
+        #img = Image(source = 'C:/Users/Kiril/PycharmProjects/MTA_pi/1_train.png', size_hint=(.5, .5), pos_hint={"center_x":.5, "center_y": .5})
+
+        #time = Label(text="X Mins",halign = "right", font_size = 40, size_hint=(.5, .5), pos_hint={"center_x": .9, "center_y": .5})
+
+
+        #this creates the overall gui layout, which is a vertical box (meaning boxes are inserted horizontally stacked vertically)
+        main_layout = BoxLayout(padding = 10, orientation = "vertical")
+
+        station_name = Label(text=f"[b]From 72nd St[/b]", markup=True, halign="left", font_size=40, size_hint=(.5, .5), pos_hint={"center_x": .15, "center_y": .5})
+        main_layout.add_widget(station_name)
+        #debugging button
+        btn = Button(text = "Refresh", background_color= random.choice(colors))
+
+        for i in range(0, len(trains_list)):
+            h_layout = BoxLayout(padding=1)
+
+            direction = Label(text="Uptown", size_hint=(.5, .5), halign="left", font_size=20,
+                              pos_hint={"center_x": .1, "center_y": .5})
+            #conditional statements put the train label picture onto
+            if trains_list[i] == '1':
+                img = Image(source = 'C:/Users/Kiril/PycharmProjects/MTA_pi/1_train.png', size_hint=(.7, .7), pos_hint={"center_x":.5, "center_y": .5})
+                h_layout.add_widget(img)
+            elif trains_list[i] == '2':
+                img = Image(source='C:/Users/Kiril/PycharmProjects/MTA_pi/2_train.png', size_hint=(.7, .7),
+                            pos_hint={"center_x": .5, "center_y": .5})
+                h_layout.add_widget(img)
+            else:
+                img = Image(source='C:/Users/Kiril/PycharmProjects/MTA_pi/3_train.png', size_hint=(.7, .7),
+                            pos_hint={"center_x": .5, "center_y": .5})
+                h_layout.add_widget(img)
+
+            h_layout.add_widget(direction)
+
+            train_time = times_list[i]
+            time = Label(text=f"{train_time} Mins", halign="right", font_size=40, size_hint=(.5, .5),
+                         pos_hint={"center_x": .9, "center_y": .5})
+            h_layout.add_widget(time)
+
+            main_layout.add_widget(h_layout)
+
+        #h_layout.add_widget(img)
+        #h_layout.add_widget(direction)
+        #h_layout.add_widget(time)
+
+
+        btn.bind(on_press=self.on_button_press)
+        main_layout.add_widget(btn)
+        #Clock.schedule_interval(self.update,1)
+
+
+
+
+        return main_layout
+
+    def on_button_press(self, instance):
+        #current = self.solution.text
+        button_text = instance.text
+
+
+        if button_text == "Refresh":
+            print("Button Pressed")
+            self.build()
+
+
+    def _update_rect(self, instance, value):
+        self.circle.pos = instance.pos
+        self.circle.size = instance.size
+
+    def get_trains(self):
+        return mta_times.get_train("72nd North")
+
 
 
 
--- mta_times.py
+++ mta_times.py
@@ -7,77 +7,92 @@ import time
 # headers={"x-api-key": "769b59cf365fb57f201e379116b64829"}
 
 api_key= "769b59cf365fb57f201e379116b64829"
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get("http://datamine.mta.info/mta_esi.php?key=769b59cf365fb57f201e379116b64829")
-
-
-feed.ParseFromString(response.content)
-
-subway_feed = protobuf_to_dict(feed) #is a dictionary
-
-realtime_data = subway_feed['entity'] #is a list
-stations = {"72nd North" : "123N", "72nd South" : "123S", "66th South" : "124S"}
-station_times = []
-route_id_list = []
-times_dict = {}
-
-def get_traintime(data, station_id):
-    for train_trip_id in data:
-        if train_trip_id.get('trip_update'):
-            train_schedule_update = train_trip_id["trip_update"]
-            train_id = train_schedule_update["trip"]
-            arrival_times = train_schedule_update["stop_time_update"]
-
-            for arrivals in arrival_times:
-                if arrivals.get("stop_id") == station_id: # make lists if stop_id matches station id
-
-                    arrival_data = arrivals["arrival"]
-                    time = arrival_data["time"] # this appends train times to list
-                    if time != None:
-                        station_times.append(time)
-
-                    route_id = train_id["route_id"] # this appends train id's in the same order as train times on a separate list
-                    if route_id != None:
-                        route_id_list.append(route_id)
 
 
 
-
-get_traintime(realtime_data,stations.get("72nd North"))
-
-#this loop creates a dictionary with train times associated with train IDs (1, 2 or 3 train)
-for i in station_times:
-    #debug
-    #print(i)
-    for k in route_id_list:
-        times_dict[i] = k
-        #Debug
-        #print(k)
-        route_id_list.remove(k)
-        break
+def get_train(station):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get("http://datamine.mta.info/mta_esi.php?key=769b59cf365fb57f201e379116b64829")
 
 
-station_times.sort()
+    feed.ParseFromString(response.content)
 
-#Debug
-#print(str(times_dict))
-#print(route_id_list)
+    subway_feed = protobuf_to_dict(feed) #is a dictionary
 
-print("current time")
-current_time = int(time.time())
-print(current_time)
+    realtime_data = subway_feed['entity'] #is a list
+    stations = {"72nd North" : "123N", "72nd South" : "123S", "66th South" : "124S"}
+    station_times = []
+    route_id_list = []
+    times_dict = {}
 
-def print_trains():
+    def get_traintime(data, station_id):
+        nonlocal station_times
+        nonlocal route_id_list
+        for train_trip_id in data:
+            if train_trip_id.get('trip_update'):
+                train_schedule_update = train_trip_id["trip_update"]
+                train_id = train_schedule_update["trip"]
+                arrival_times = train_schedule_update["stop_time_update"]
+
+                for arrivals in arrival_times:
+                    if arrivals.get("stop_id") == station_id:  # make lists if stop_id matches station id
+
+                        arrival_data = arrivals["arrival"]
+                        time = arrival_data["time"]  # this appends train times to list
+                        if time != None:
+                            station_times.append(time)
+
+                        route_id = train_id[
+                            "route_id"]  # this appends train id's in the same order as train times on a separate list
+                        if route_id != None:
+                            route_id_list.append(route_id)
+
+    get_traintime(realtime_data,stations.get(station)) #default is "72nd North"
+
+    #this loop creates a dictionary with train times associated with train IDs (1, 2 or 3 train)
+    for i in station_times:
+        #debug
+        #print(i)
+        for k in route_id_list:
+            times_dict[i] = k
+            #Debug
+            #print(k)
+            route_id_list.remove(k)
+            break
+
+
+    station_times.sort()
+
+    #Debug
+    #print(str(times_dict))
+    #print(route_id_list)
+
+    print("current time")
+    current_time = int(time.time())
+    print(current_time)
+    return print_trains(station_times, current_time, times_dict)
+
+def print_trains(station_times, current_time, times_dict):
+    print(times_dict)
+    trains_list = []
+    times_list = []
     for i in range(0,5):
         if (int(((station_times[i] - current_time) / 60 )) > -1):
             print("------------------------")
             print(f"{times_dict.get(station_times[i])} Train") #this prints the train ID with respect to the time it departs
+            trains_list.append(times_dict.get(station_times[i]))
             print(" ")
             print(f"{int(((station_times[i] - current_time) / 60 ))} Minutes") #this prints the train's departure time
+            times_list.append(int(((station_times[i] - current_time) / 60 )))
             print("------------------------")
 
-print_trains()
+    #print(trains_list)
+    #print(times_list)
+    return trains_list, times_list
 
+print(get_train("72nd North"))
+
+"""
 def debug():
     train_first = int(((station_times[0] - current_time) / 60 ))
     train_first_id = times_dict.get(station_times[0])
@@ -108,3 +123,4 @@ def debug():
     print("next train: ")
     print(f"{train_fourth_id} Train")
     print(train_arrives_at_station_next)
+    """
